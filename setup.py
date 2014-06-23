7750888956fe4f0e7e9ee2ce09e17a665aa583b8
Uche Ogbuji <uche@ogbuji.net>
2014-06-22 21:40:47 -0600

    Push package to PyPI. 0.2.1 release.

--- setup.py
+++ setup.py
@@ -3,15 +3,34 @@
 from distutils.core import setup
 #from lib import __version__
 
-setup(name = "versa",
-      #version = __version__,
-      version = "0.2",
-      description="Versa model for Web resources and relationships. Think of it as an evolution of Resource Description Framework (RDF) that's at once simpler and more expressive.",
-      author='Uche Ogbuji',
-      author_email='uche@ogbuji.net',
-      url='http://uche.ogbuji.net',
-      package_dir={'versa': 'tools/py'},
-      packages=['versa', 'versa.driver', 'versa.reader', 'versa.writer'],
-      scripts=['tools/exec/build_model_site', 'tools/exec/parse_versa'],
-      #package_data={'akara': ["akara.conf"]},
-      )
+versionfile = 'tools/py/version.py'
+exec(compile(open(versionfile, "rb").read(), versionfile, 'exec'), globals(), locals())
+__version__ = '.'.join(version_info)
+
+setup(
+    name = "versa",
+    version = __version__,
+    #version = "0.2",
+    description="Versa model for Web resources and relationships. Think of it as an evolution of Resource Description Framework (RDF) that's at once simpler and more expressive.",
+    author='Uche Ogbuji',
+    author_email='uche@ogbuji.net',
+    url='http://uche.ogbuji.net',
+    package_dir={'versa': 'tools/py'},
+    packages=['versa', 'versa.driver', 'versa.reader', 'versa.writer'],
+    scripts=['tools/exec/build_model_site', 'tools/exec/parse_versa'],
+    keywords = ["web", "data"],
+    #scripts=['exec/exhibit_agg', 'exec/exhibit_lint'],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        #"Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Internet :: WWW/HTTP",
+    ],
+    long_description = '''
+    '''
+    )
