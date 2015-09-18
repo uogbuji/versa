e0b7fbdff129951547247c5e84f4b47fa1c7d768
Uche Ogbuji <uche@ogbuji.net>
2015-09-18 11:28:52 -0600

    Put replace_entity_resource & duplicate_statements where they belong

--- tools/py/util.py
+++ tools/py/util.py
@@ -9,7 +9,7 @@ Utilities to help deal with constructs expressed in Versa
 import json
 from collections import OrderedDict
 
-from versa import I, ORIGIN, RELATIONSHIP, TARGET
+from versa import I, ORIGIN, RELATIONSHIP, TARGET, ATTRIBUTES
 from versa import init_localization
 init_localization()
 from versa import VERSA_BASEIRI
@@ -59,6 +59,55 @@ def column(m, linkpart):
             yield val
 
 
+#XXX Could use a factory defined on in_m to create out_m, or do we want to use this approach to support append?
+def replace_values(in_m, out_m, map_from=(), map_to=()):
+    '''
+    Make a copy of a model with one value replaced with another
+    '''
+    for link in in_m.match():
+        new_link = list(link)
+        if map_from:
+            if link[ORIGIN] in map_from: new_link[ORIGIN] = map_to[map_from.index(link[ORIGIN])]
+        new_link[ATTRIBUTES] = link[ATTRIBUTES].copy()
+        out_m.add(*new_link)
+    return
+
+
+def replace_entity_resource(model, oldres, newres):
+    '''
+    Replace one entity in the model with another with the same links
+
+    :param model: Versa model to be updated
+    :param oldres: old/former resource IRI to be replaced
+    :param newres: new/replacement resource IRI
+    :return: None
+    '''
+    oldrids = set()
+    for rid, link in model:
+        if link[ORIGIN] == oldres or link[TARGET] == oldres or oldres in link[ATTRIBUTES].values():
+            oldrids.add(rid)
+            new_link = (newres if o == oldres else o, r, newres if t == oldres else t, dict((k, newres if v == oldres else v) for k, v in a.items()))
+            model.add(*new_link)
+    model.delete(oldrids)
+    return
+
+
+def duplicate_statements(model, oldorigin, neworigin, rfilter=None):
+    '''
+    Take links with a given origin, and create duplicate links with the same information but a new origin
+
+    :param model: Versa model to be updated
+    :param oldres: resource IRI to be duplicated
+    :param newres: origin resource IRI for duplication
+    :return: None
+    '''
+    for link in model.match(oldorigin):
+        o, r, t, a = link
+        if rfilter is None or rfilter(o, r, t, a):
+            model.add(I(neworigin), r, t, a)
+    return
+
+
 def jsonload(model, fp):
     '''
     Load Versa model dumped into JSON form, either raw or canonical
@@ -89,6 +138,7 @@ def jsondump(model, fp):
         links_ser.append(json.dumps(link))
     fp.write(',\n'.join(links_ser))
     fp.write(']')
+
 
 class OrderedJsonEncoder(json.JSONEncoder):
     '''
