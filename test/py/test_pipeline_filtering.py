e6b04fe3fdf31e6066186950ed812cb4aeec7a48
Uche Ogbuji <uche@ogbuji.net>
2021-05-03 14:10:06 -0600

    [#24] Add COPY mode to materialize() action function, with a test case

---
+++ test/py/test_pipeline_filtering.py
@@ -0,0 +1,112 @@
+# test_pipeline_filtering.py (use py.test)
+'''
+Tests of using the pipeline to trim/reduce a graph
+
+Note: to see stdout, stderr & logging regardless of outcome:
+
+py.test -s test/py/test_pipeline.py
+
+'''
+
+import os
+
+# Requires pytest-mock
+import pytest
+
+from versa import I, VERSA_BASEIRI, VTYPE_REL, VLABEL_REL, ORIGIN, RELATIONSHIP, TARGET
+from versa import util
+from versa.driver.memory import newmodel
+from versa.serial import csv, literate, mermaid
+from versa.pipeline import *
+
+INPUT_GRAPH_1 = '''\
+# @docheader
+
+* @iri:
+    * @base: http://example.org/records/
+    * @schema: https://schema.org/
+
+# black-star [MusicAlbum]
+
+* name: Mos Def & Talib Kweli Are Black Star
+* byArtist: <md>
+* byArtist: <tk>
+* inLanguage: en
+
+# train [MusicAlbum]
+
+* name: Train of Thought
+* byArtist: <tk>
+* byArtist: <ht>
+* inLanguage: en
+
+# md [Person]
+
+* alternateName: Mos Def
+* name: Yasiin Bey
+* birthDate: 1973-12-11
+
+# tk [Person]
+
+* alternateName: Talib Kweli
+* name: Talib Kweli Greene
+* birthDate: 1975-10-03
+
+# ht [Person]
+
+* alternateName: Hi-Tek
+* name: Tony Cottrell
+* birthDate: 1976-05-05
+'''
+
+
+@pytest.fixture
+def expected_modout1():
+    modout = newmodel()
+    #literate.parse('''
+
+    #''', modout)
+    return modout
+
+MB_NS = I('https://musicbrainz.org/doc/MusicBrainz_Database/Schema/')
+SCH_NS = I('https://schema.org/')
+
+def test_mosdef_only(testresourcepath, expected_modout1):
+    modin = newmodel()
+    literate.parse(INPUT_GRAPH_1, modin)
+
+    R_TYP = MB_NS('Release')
+    RG_TYP = MB_NS('ReleaseGroup')
+    A_TYP = MB_NS('Artist')
+    DOC_NS = I('http://example.org/records/')
+
+    modin = newmodel()
+    literate.parse(INPUT_GRAPH_1, modin)
+
+    FINGERPRINT_RULES = {
+        SCH_NS('MusicAlbum'): ( 
+            if_(contains(follow(SCH_NS('byArtist')), DOC_NS('md')),
+                materialize(COPY())
+            )
+        ),
+
+        SCH_NS('Person'): ( 
+            materialize(COPY())
+        ),
+    }
+
+    ppl = generic_pipeline(FINGERPRINT_RULES, {}, {})
+
+    modout = ppl.run(input_model=modin)
+    # Use -s to see this
+    print('='*10, 'test_mosdef_only', '='*10)
+    literate.write(modout)
+    # import pprint; pprint.pprint(list(iter(modout)))
+
+    assert len(modout) == 17
+    assert len(list(util.all_origins(modout, only_types={SCH_NS('MusicAlbum')}))) == 1
+    assert len(list(util.all_origins(modout, only_types={SCH_NS('Person')}))) == 3
+
+
+if __name__ == '__main__':
+    raise SystemExit("use py.test")
